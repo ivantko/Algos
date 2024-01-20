6eb9e00bd68cad9a8011253d1bcbfeec398e55b6
Ivan Tovpeko <ivan.tko35@gmail.com>
2024-01-20 17:50:50 -0600

    completed 2nd version of solving

--- ValidateSubsequence.js
+++ ValidateSubsequence.js
@@ -1,12 +1,12 @@
-// fucntion isValidateSubsequence(array, sequence) {
-    // let arrIdx = 0;
-    // let seqIdx = 0;
-    // while (arrIdx < array.length && seqIdx < sequence.length) {
-    //     if (array[arrIdx] === sequence[seqIdx]) seqIdx++;
-    //     arrIdx++; 
-    // }
-
-    // console.log(
+// function isValidSubsequence(array, sequence) {
+//     let arrIdx = 0;
+//     let seqIdx = 0;
+//     while (arrIdx < array.length && seqIdx < sequence.length) {
+//         if (array[arrIdx] === sequence[seqIdx]) seqIdx++;
+//         arrIdx++; 
+//     }
+//     return seqIdx === sequence.length;
+// }
 
     function isValidSubsequence (array, sequence) {
         let mainPointer = 0;
@@ -21,3 +21,4 @@
     }
 
     console.log(isValidSubsequence([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10])); // Should return true
+
