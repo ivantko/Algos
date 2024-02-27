a8a419bb883d2733370e9e6bc61c973214130198
ivantko <146136372+ivantko@users.noreply.github.com>
2024-02-27 13:03:11 +0000

    practiced validateSubseq

--- validateSubseqPrac.js
+++ validateSubseqPrac.js
@@ -24,21 +24,32 @@
 //     return seqPointer === sequence.length;
 // }
 
-function isValidSubseq(array1, array2) {
-    //start pointers at index 0 and iterater with while loop
-    let mainPointer = 0;
-    let seqPointer = 0;
-    while (mainPointer < array1.length) {
-        if (array1[mainPointer] === array2[seqPointer]) {
-            seqPointer++;
-        }
-        mainPointer++;
-    }
-    return seqPointer === array2.length
+// function isValidSubseq(array1, array2) {
+//     //start pointers at index 0 and iterater with while loop
+//     let mainPointer = 0;
+//     let seqPointer = 0;
+//     while (mainPointer < array1.length) {
+//         if (array1[mainPointer] === array2[seqPointer]) {
+//             seqPointer++;
+//         }
+//         mainPointer++;
+//     }
+//     return seqPointer === array2.length
          
-    }
+//     }
 
-
+// function isValidSubseq(array, sequence) {
+//     let mainPointer = 0;
+//     let seqPointer = 0;
+//     while (mainPointer < array.length) {
+//     //iterate over array index until pointer reaches end of main array while checking if it matches pointer of second array
+//     if (array[mainPointer] === sequence[seqPointer]) {
+//         seqPointer ++;
+//     }
+//     mainPointer++;
+//     }
+//     return seqPointer === sequence.length
+// }
 
 
 
